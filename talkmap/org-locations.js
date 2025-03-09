379adf54f29b74ac08af3a0ab2caaa68d357a083
Naels Krausch <nkrausch@gmx.de>
2025-03-09 17:25:56 +0100

    Updated about section and map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,42 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Standardized Interfaces - Is This the Key to Unifying Individuals from Different Fields Working Collaboratively?<br />FUTURELABS Symposium; Basel, Switzerland",
+    47.5581077,
+    7.5878261
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Automated Model-Based Optimization of Difficult-to-Express Protein Expression in a Robotic Facility<br />23rd Annual Peptalk; San Diego, CA, USA",
+    32.7174202,
+    -117.162772
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Online model calibration - the key to successful high throughput bioprocess automation<br />13th European Congress of Chemical Engineering and 6th European Congress of Applied Biotechnology; Online",
+    43.59047185,
+    3.8595132132013186
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Model-based optimal control of parallel mini-bioreactors<br />7th BioProScale Symposium; Berlin, Germany",
+    52.510885,
+    13.3989367
+  ],
+  [
+    "Fast forward bioprocess development: A holistic approach of high-throughput model-based screening for ELP production<br />Dechema Himmelfahrtstagung on Bioprocess Engineering; Online",
+    43.59047185,
+    3.8595132132013186
+  ],
+  [
+    "High-throughput screening of optimal process conditions for ELP production via model predictive control<br />2021 AIChE Annual Meeting; Boston, MA, USA",
+    42.3554334,
+    -71.060511
+  ],
+  [
+    "Handling nonlinearities and uncertainties of fed-batch cultivations with difference of convex functions tube MPC<br />34th European Symposium on Computer Aided Process Engineering; Florence, Italy",
+    43.7697955,
+    11.2556404
+  ],
+  [
+    "'The FAIRest dataset in chemistry': UV/Vis spectra acquired while monitoring reaction progress of thymidine phosphorolysis<br />JCF Spring Symposium; Hannover, Germany",
+    52.3744779,
+    9.7385532
   ]
 ];
